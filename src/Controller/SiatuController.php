0b4e61bcc7854579c3b7f1321e05d09e16d18654
danilopbh <danilo.dlima@pbh.gov.br>
2024-10-14 15:36:44 -0300

    inicio padrao dto

---
+++ src/Controller/SiatuController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use App\Resource\SiatuResource;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\ContribuinteSupp;
+use App\Entity\CertidaoDividaSupp;
+use App\Rules\CertidaoDividaRules;
+
+class SiatuController extends AbstractController
+{
+   
+}
+
+?>
