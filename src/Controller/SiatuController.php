b562ea756500e5dcee869188ab8f94ab83f90c83
danilopbh <danilo.dlima@pbh.gov.br>
2024-10-24 11:42:54 -0300

    demais validacoes

--- src/Controller/SiatuController.php
+++ src/Controller/SiatuController.php
@@ -43,6 +43,7 @@ class SiatuController extends AbstractController
         $mensagemErro = '';
 
         foreach ($contribuintes as $contribuinteDTO) {
+
             $contribuenteErrors = (new ContribuinteRules())->validate((array)$contribuinteDTO);
             if (!empty($contribuenteErrors)) {
                 $allErrors[] = $contribuenteErrors;
@@ -50,63 +51,76 @@ class SiatuController extends AbstractController
                 $mensagemErro = 'Erro ao validar contribuinte ' . $contribuinteDTO->nome;   // Variável pa
                 continue;
             }
+            $contribuinteSupp = $this->entityManager->getRepository(ContribuinteSupp::class)->findOneBy(['cpf' => $contribuinteDTO->cpf]);
 
-            $contribuinte = new ContribuinteSupp();
-            $contribuinte->setNome($contribuinteDTO->nome);
-            $contribuinte->setCpf($contribuinteDTO->cpf);
-            $contribuinte->setEndereco($contribuinteDTO->endereco);
-            $contribuinte->setIdContribuinteSiatu($contribuinteDTO->id_contribuinte_siatu);
-            $this->entityManager->persist($contribuinte);
-            $this->entityManager->flush();
 
-            $contribuinteId = $contribuinte->getId();
 
+            if (!$contribuinteSupp){
+             
+                $contribuinte = new ContribuinteSupp();
+                $contribuinte->setNome($contribuinteDTO->nome);
+                $contribuinte->setCpf($contribuinteDTO->cpf);
+                $contribuinte->setEndereco($contribuinteDTO->endereco);
+                $contribuinte->setIdContribuinteSiatu($contribuinteDTO->id_contribuinte_siatu);
+                $this->entityManager->persist($contribuinte);
+                $this->entityManager->flush();
+            
+
+                $contribuinteId = $contribuinte->getId();
+
+            } else {
+
+                $contribuinteId = $contribuinteSupp->getId();
+            }
+
+            
             foreach ($certidaoDivida as $certidaoDTO) {
                 $validationErrors = CertidaoDividaRules::validate($certidaoDTO);
-                
+
                 // Validação de erros
-                if (!empty($validationErrors)) {   
+                if (!empty($validationErrors)) {
                     $allErrors[] = [
                         'certidaoId' => $certidaoDTO->id,
                         'erros' => $validationErrors
                     ];
-            
+
                     // Atualiza o status e a mensagem de erro corretamente
                     $status = 'erro';
-                    $mensagemErro = 'Erro de validação na certidão ' . $certidaoDTO->id;
-            
+                    $mensagemErro = 'Erro de validacao na certidao ' . $certidaoDTO->id;
+
                     // Notificar SIATU imediatamente em caso de erro de validação
                     $this->notificarSiatu($status, $mensagemErro, $allErrors);
-            
+
                     // Retorna a resposta de erro
                     return new JsonResponse([
                         'status' => 'error',
                         'codigo' => '1',
-                        'message' => 'Erro de validação na certidão',
+                        'message' => 'Erro de validação na certidao',
                         'detalhes' => $validationErrors
                     ], Response::HTTP_BAD_REQUEST);
                 }
-            
+             
                 // Verifica se o contribuinte corresponde à certidão
                 if ($certidaoDTO->id_contribuinte_siatu == $contribuinteDTO->id) {
+                
                     $contribuinteSupp = $this->entityManager->getRepository(ContribuinteSupp::class)->find($contribuinteId);
-            
+                //    dd($contribuinteId);
                     // Verifica se a certidão de dívida já existe
                     $certidaoExistente = $this->entityManager->getRepository(CertidaoDividaSupp::class)
                         ->findOneBy(['id_certidao_divida_siatu' => $certidaoDTO->id]);
-            
+
                     if ($certidaoExistente) {
                         // Atualiza o status e a mensagem de erro para duplicata de CDA
                         $status = 'erro';
-                        $mensagemErro = 'A CDA com o número ' . $certidaoDTO->id . ' já existe no banco de dados.';
-                        
+                        $mensagemErro = 'A CDA com o numero ' . $certidaoDTO->id . ' ja existe no banco de dados.';
+
                         // Notificar SIATU imediatamente para duplicata
                         $this->notificarSiatu($status, $mensagemErro, $allErrors);
-            
+
                         // Continua para a próxima certidão (se for o comportamento desejado)
                         continue;
                     }
-            
+
                     // Processa e persiste a nova certidão se não houver erros
                     $certidao = new CertidaoDividaSupp();
                     $certidao->setContribuinteSupp($contribuinteSupp);
@@ -118,14 +132,13 @@ class SiatuController extends AbstractController
                     $certidao->setDataVencimento($certidaoDTO->dataVencimento);
                     $certidao->setIdCertidaoDividaSiatu($certidaoDTO->id);
                     $certidao->setSituacao($certidaoDTO->situacao);
-            
+
                     $this->entityManager->persist($certidao);
                 }
             }
 
             // Após a execução, faz o flush
             $this->entityManager->flush();
-        
         }
 
 
@@ -152,18 +165,18 @@ class SiatuController extends AbstractController
                 'status' => $status,
                 'mensagem' => $status === 'sucesso' ? 'Importacao bem-sucedida' : 'Erro na importacao',
             ];
-    
+
             // Apenas incluir os erros se o status for 'erro'
             if ($status === 'erro') {
                 $data['Erro'] = $mensagemErro;
                 $data['DetalhesErro'] = $allErrors;
             }
-    
+
             // Envia o POST para o SIATU (coloque aqui a chamada real quando estiver pronta)
             /*$this->httpClient->request('POST', 'http://siatu/api/endpoint', [
                 'json' => $data,
             ]);*/
-    
+
             echo 'Siatu notificado';
             $json = json_encode($data);
             echo $json;
@@ -175,5 +188,23 @@ class SiatuController extends AbstractController
             ]);
         }
     }
-    
+
+
+
+
+    private function adicionarCDAAoContribuinte(ContribuinteSupp $contribuinte, $certidaoDTO)
+    {
+        $certidao = new CertidaoDividaSupp();
+        $certidao->setContribuinteSupp($contribuinte);
+        $certidao->setValor($certidaoDTO->valor);
+        $certidao->setDescricao($certidaoDTO->descricao);
+        $certidao->setPdfDivida($certidaoDTO->pdfDivida);
+        $certidao->setDataSituacao($certidaoDTO->dataSituacao);
+        $certidao->setIdContribuinteSiatu($certidaoDTO->id_contribuinte_siatu);
+        $certidao->setDataVencimento($certidaoDTO->dataVencimento);
+        $certidao->setIdCertidaoDividaSiatu($certidaoDTO->id);
+        $certidao->setSituacao($certidaoDTO->situacao);
+
+        $this->entityManager->persist($certidao);
+    }
 }
